db277b00dddbfbf5ad36071e0eee28244ee40187
l-u-ka <luka12208@gmail.com>
2022-12-18 01:57:12 +0400

    add block and template

--- WeatherApp/Block/Index.php
+++ WeatherApp/Block/Index.php
@@ -1,5 +1,32 @@
 <?php
+
 namespace Luka\WeatherApp\Block;
-class Index extends \Magento\Framework\View\Element\Template
+
+use Magento\Framework\View\Element\Template;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+
+class Index extends Template
 {
+    protected $_coreRegistry;
+    public function __construct(Context $context, Registry $coreRegistry, array $data = [])
+    {
+        $this->_coreRegistry = $coreRegistry;
+        parent::__construct($context, $data);
+    }
+
+    public function getFormAction()
+    {
+        return $this->getUrl('weather/index/save', ['_secure' => true]);
+    }
+
+    public function getPdfAction()
+    {
+        return $this->getUrl('weather/index/pdf', ['_secure' => true]);
+    }
+
+    public function getList()
+    {
+        return $this->_coreRegistry->registry('data');
+    }
 }
